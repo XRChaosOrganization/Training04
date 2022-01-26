8dc1695142e4bc37a860b9acb968c11ee9210445
Ben <benjamin.matheron@hotmail.fr>
2022-01-26 15:34:06 +0100

    [Tech] Audio Manager

--- Training_04/Assets/Scripts/System/AudioManager.cs
+++ Training_04/Assets/Scripts/System/AudioManager.cs
@@ -2,16 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager am;
+
+    public enum SFX { UI_Confirm, UI_Navigate, Player_Tongue, Player_Death, Gameplay_Fly, Gameplay_Win, Gameplay_GameOver}
+    
+
+    public List<AudioSource> ui;
+    public List<AudioSource> player;
+    public List<AudioSource> gameplay;
 
     private void Awake()
     {
         am = this;
 
-
-
         DontDestroyOnLoad(this.gameObject);
+        
+    }
+
+    public void PlaySFX(SFX sfx)
+    {
+        switch (sfx)
+        {
+            case SFX.Gameplay_Fly:
+                gameplay[0].Play();
+                break;
+            case SFX.Gameplay_GameOver:
+                gameplay[2].Play();
+                break;
+            case SFX.Gameplay_Win:
+                gameplay[1].Play();
+                break;
+            case SFX.Player_Death:
+                player[1].Play();
+                break;
+            case SFX.Player_Tongue:
+                player[0].Play();
+                break;
+            default:
+                break;
+        }
     }
 }
